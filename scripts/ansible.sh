d3b2c9a7078722811951f00062ded4b5a268e227
Dany Davila <danydavila@gmail.com>
2019-10-26 17:11:13 -0400

    Assume yes -y to adding Launchpad Ansible repository

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -3,7 +3,7 @@
 # Install Ansible repository.
 apt -y update && apt-get -y upgrade
 apt -y install software-properties-common
-apt-add-repository ppa:ansible/ansible
+apt-add-repository -y ppa:ansible/ansible
 
 # Install Ansible.
 apt -y update
